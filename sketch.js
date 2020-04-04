a424352682f6ffef07494c74b76e2618aa1d14b7
Suryansh2010 <suryansh2010@icloud.com>
2020-04-04 16:18:42 +0400

    s

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    slingshot = new SlingShot(bird.body,{x:200,y:100});
 
 }
 
@@ -60,5 +62,15 @@ function draw(){
     log5.display();
 
     bird.display();
+    slingshot.display();
+
     platform.display();
+}
+
+function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
